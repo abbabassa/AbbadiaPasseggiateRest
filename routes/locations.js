ac92d2428d948131676a0eabd5e0b17444d809d1
Paolo Bassanese <abbabassagmail.com>
2019-05-04 19:27:20 +0200

    test commit

--- routes/locations.js
+++ routes/locations.js
@@ -4,6 +4,7 @@ var pool= require('../pg/PostgresPool')
 var locationsQueries = require('../pg/queries/locations')
 
 
+
 router.get('/map/:locale/:id', function(req, res, next) {
   pool.query(locationsQueries.getDescriptionById(req.params.locale),
    [req.params.id])
@@ -22,6 +23,26 @@ router.get('/map/:locale/:id', function(req, res, next) {
   })
   .catch(err=>{ console.log(err); next(err)});
 }, function(req,res,next){
+  res.json(res.locals.locRes);
+});
+
+router.get('/', function(req, res, next) {
+  res.locals.locRes= 
+  [
+    {
+    Name:"prova",
+    Id:1,
+    Photos: []
+    },
+    {
+      Name:"prova2",
+      Id:2,
+      Photos: [{
+        Name:"photo1",
+        Path:"nonDovreiSaperloIo"
+      }]
+      }
+   ]
   res.json(res.locals.locRes);
 });
 
