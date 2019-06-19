8f719a8bae0aa4de7d8731714cb6f28c384aa7b7
Paolo Bassanese <abbabassagmail.com>
2019-06-19 11:04:32 +0200

    added images

--- routes/locations.js
+++ routes/locations.js
@@ -20,9 +20,24 @@ router.get('/map/:locale/:id', function(req, res, next) {
     res.locals.locRes.trailData= qResult.rows ;
     next()
   })
-  .catch(err=>{ console.log(err); next(err)});
-}, function(req,res,next){
+  .catch(err=> next(err));
+}, 
+  getPhotoData,
+function(req,res,next){
   res.json(res.locals.locRes);
 });
+
+
+
+function getPhotoData(req,res, next)
+{
+  pool.query(locationsQueries.getPhotosByLocationId(req.params.locale),
+    [req.params.id])
+  .then(qResult=>{
+    res.locals.locRes.imagesData= qResult.rows ;
+    next()
+  })
+  .catch(err=>{ next(err)});
+}
 
 module.exports = router;
