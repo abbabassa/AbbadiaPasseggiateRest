6303fccbf14a0fa123f25173195286ef1886b8b3
Paolo Bassanese <abbabassagmail.com>
2019-08-29 11:23:37 +0200

    prova

--- routes/serve-application.js
+++ routes/serve-application.js
@@ -3,12 +3,31 @@ var router = express.Router();
 var path = require('path');
 
 // basepath to public/dist to download js and css files (according to application basepath)
-router.use('/', express.static(path.join(__dirname, '../public/dist')));
 router.use('/en', express.static(path.join(__dirname, '../public/dist/en')));
+router.use('/', express.static(path.join(__dirname, '../public/dist')));
+
 
 // express seems to have some problem with Angular notation for optional parameters and secondary outlet.
 // if the path is suitable for parameters in the map/secondary outlet in the map, then the index.html will be loaded.
 // Then the parameters/secondary outlets path will be resolved by Angular router
+router.use(function(req,res,next) {
+  const regex = /\/en\/map(?:;\w+=\w+)*(?:\(\w+:\w+(?:\/\d)*\))*/gm;
+  var appCompatiblePath =   req.originalUrl.match(regex);
+  if(appCompatiblePath && appCompatiblePath.length > 0)
+  {
+    next()
+  }
+  else
+  {
+    next('route');
+  }
+  
+}, 
+function(req,res,next)
+{
+  res.sendFile(path.join(__dirname, '../public/dist/en/index.html'))
+});
+
 router.use(function(req,res,next) {
   const regex = /\/map(?:;\w+=\w+)*(?:\(\w+:\w+(?:\/\d)*\))*/gm;
   var appCompatiblePath =   req.originalUrl.match(regex);
@@ -28,23 +47,7 @@ function(req,res,next)
 });
 
 
-router.use(function(req,res,next) {
-  const regex = /\/en\/map(?:;\w+=\w+)*(?:\(\w+:\w+(?:\/\d)*\))*/gm;
-  var appCompatiblePath =   req.originalUrl.match(regex);
-  if(appCompatiblePath && appCompatiblePath.length > 0)
-  {
-    next()
-  }
-  else
-  {
-    next('route');
-  }
-  
-}, 
-function(req,res,next)
-{
-  res.sendFile(path.join(__dirname, '../public/dist/en/index.html'))
-});
+
 
 module.exports = router;
 
